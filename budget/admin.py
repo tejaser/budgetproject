4145a706268f3156f95b13ba9b4d44bc9ac8087d
Tejas Pancholi <tejas.er@outlook.com>
2018-08-21 06:59:38 +0530

    created models necessary and add expense page.

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Category, Expense
+
+admin.site.register(Project)
+admin.site.register(Category)
+admin.site.register(Expense)
--- budget/views.py
+++ budget/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+from django.views.generic import CreateView
 
 def project_list(request):
     return render(request, 'budget/project-list.html')
 
 
 def project_detail(request, project_slug):
-    # fetch correct project
-    return render(request, 'budget/project-detail.html')
+    project = get_object_or_404(Project, slug=project_slug)
+    # expense_list = project.expenses
+    return render(request, 'budget/project-detail.html',{'project': project, 'expense_list': project.expenses.all()})
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
